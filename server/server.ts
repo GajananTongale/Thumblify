4afe344ba6dbe32c7b3e2f059b4c6b7b0f69134a
Gajanan Rajendra Tongale <gajutongale@gmail.com>
2026-01-09 14:05:47 +0530

    session and cookie properties updated

--- server/server.ts
+++ server/server.ts
@@ -28,6 +28,8 @@ app.use(cors({
     credentials: true,
 }))
 
+app.set('trust proxy', 1);
+
 app.use(session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
@@ -36,7 +38,7 @@ app.use(session({
         maxAge: 60 * 60 * 1000 * 24 * 7,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV === 'production'? 'none':'lax',
         path: '/'
     },
     store: MongoStore.create({
